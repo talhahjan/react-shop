52b606dd63ac0634ba428c958605b1a97b8615fc
Muhammad Khalid <talhah.jan@gmail.com>
2022-08-08 16:21:46 -0700

    social login packages added 3

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,21 +1,36 @@
- 
 import React from 'react';
 import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
+import InstagramLogin from 'react-instagram-login';
 
 
-const responseFacebook = (response) => {
-    console.log(response);
-  }
+
+const responseInstagram = (response) => {
+  console.log(response);
+}
+
 
 export const SocialLogin = () => {
   return (
+  <>
     <FacebookLogin
-    appId="377398063014920"
-    autoLoad={true}
-    fields="name,email,picture"
-    callback={responseFacebook}
-    cssClass="my-facebook-button-class"
-    icon="fa-facebook"
+    clientId="377398063014920"
+    buttonText="Facebook Login"
+    onSuccess={responseInstagram}
+    onFailure={responseInstagram}
   />
+  <InstagramLogin
+  clientId="396795559223412"
+  buttonText="Instagram Login"
+  onSuccess={responseInstagram}
+  onFailure={responseInstagram}
+/>
+    <GoogleLogin
+    clientId="924551858846-j9h4o2dv11p6bbt9chrm99rg0uhjhcnn.apps.googleusercontent.com"
+    buttonText="Google Login"
+    onSuccess={responseInstagram}
+    onFailure={responseInstagram}
+  />
+  </>
   )
 }
