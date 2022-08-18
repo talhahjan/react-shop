9c109247250b4de989cb0a6fa238cd372a9034bd
Muhammad Khalid <talhah.jan@gmail.com>
2022-08-18 05:14:58 -0700

    facebook sdk implemented

--- src/pages/auth/facebook/login.js
+++ src/pages/auth/facebook/login.js
@@ -29,35 +29,6 @@ const FacebookLoginBtn = (props) => {
     });
   }
 
-  const LoginInbackend = async (response) => {
-    let names = response.name.split(" ");
-    let user = {
-      provider: "facebook",
-      provider_id: response.id,
-
-      email: response.email,
-      first_name: names[0],
-      last_name: names[1],
-      avatar: response.picture.data.url,
-      jwt: response.accessToken,
-    };
-
-    await axios
-      .get(`api/login/facebook`, user)
-      .then((res) => {
-        if ((res.statusText = "Logged in success")) {
-          localStorage.setItem("token", res.data.authorisation.token);
-          console.log(res);
-          window.location = process.env.REACT_APP_HOME_PAGE;
-        } else {
-          console.log(res);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   useEffect(() => {
     window.fbAsyncInit = () => {
       window.FB.init({
--- src/pages/test.js
+++ src/pages/test.js
@@ -3,12 +3,41 @@ import FacebookLoginBtn from "../pages/auth/facebook/login";
 export const FacebookLogin = () => {
   const onSuccess = async (response) => {
     console.log(response);
+    LoginInbackend(response);
   };
 
   const onError = async (error) => {
     console.log(error);
   };
 
+  const LoginInbackend = async (response) => {
+    let names = response.name.split(" ");
+    let user = {
+      provider: "facebook",
+      provider_id: response.id,
+
+      email: response.email,
+      first_name: names[0],
+      last_name: names[1],
+      avatar: response.picture.data.url,
+      jwt: response.accessToken,
+    };
+
+    await axios
+      .get(`api/login/facebook`, user)
+      .then((res) => {
+        if ((res.statusText = "Logged in success")) {
+          localStorage.setItem("token", res.data.authorisation.token);
+          console.log(res);
+          window.location = process.env.REACT_APP_HOME_PAGE;
+        } else {
+          console.log(res);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <FacebookLoginBtn
       cssClass="btn btn-primary "
