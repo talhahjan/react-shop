7f6f388de1d1ad776b4f59dc9a4a0b021f46dd04
Muhammad Khalid <talhah.jan@gmail.com>
2022-08-18 05:45:27 -0700

    facebook sdk implemented

--- src/pages/auth/facebook/login.js
+++ src/pages/auth/facebook/login.js
@@ -5,8 +5,6 @@ const FacebookLoginBtn = (props) => {
   const appID = props.appID
     ? props.appID
     : process.env.REACT_APP_FACEBOOK_APP_iD;
-  const onSuccess = props.onSuccess;
-  const onError = props.onError;
   function login() {
     if (FB.getAuthResponse()) {
       return onError("user All Ready Logged in");
@@ -28,6 +26,44 @@ const FacebookLoginBtn = (props) => {
       onSuccess(response);
     });
   }
+
+  const onSuccess = async (response) => {
+    console.log(response);
+    LoginInbackend(response);
+  };
+
+  const onError = async (error) => {
+    console.log(error);
+  };
+
+  const LoginInbackend = async (response) => {
+    let names = response.name.split(" ");
+    let user = {
+      provider: "facebook",
+      provider_id: response.id,
+
+      email: response.email,
+      first_name: names[0],
+      last_name: names[1],
+      avatar: response.picture.data.url,
+      jwt: response.accessToken,
+    };
+
+    await axios
+      .post(`api/login/facebook`, user)
+      .then((res) => {
+        if ((res.statusText = "Logged in success")) {
+          localStorage.setItem("token", res.data.authorisation.token);
+          console.log(res);
+          window.location = process.env.REACT_APP_HOME_PAGE;
+        } else {
+          console.log(res);
+        }
+      })
+      .catch((error) => {
+        throw new Error("error occured while login user from back-end server");
+      });
+  };
 
   useEffect(() => {
     window.fbAsyncInit = () => {
--- src/pages/test.js
+++ src/pages/test.js
@@ -2,51 +2,8 @@ import React from "react";
 import FacebookLoginBtn from "../pages/auth/facebook/login";
 import axios from "axios";
 export const FacebookLogin = () => {
-  const onSuccess = async (response) => {
-    console.log(response);
-    LoginInbackend(response);
-  };
-
-  const onError = async (error) => {
-    console.log(error);
-  };
-
-  const LoginInbackend = async (response) => {
-    let names = response.name.split(" ");
-    let user = {
-      provider: "facebook",
-      provider_id: response.id,
-
-      email: response.email,
-      first_name: names[0],
-      last_name: names[1],
-      avatar: response.picture.data.url,
-      jwt: response.accessToken,
-    };
-
-    console.log(user.email);
-
-    await axios
-      .post(`api/login/facebook`, user)
-      .then((res) => {
-        if ((res.statusText = "Logged in success")) {
-          localStorage.setItem("token", res.data.authorisation.token);
-          console.log(res);
-          window.location = process.env.REACT_APP_HOME_PAGE;
-        } else {
-          console.log(res);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
   return (
-    <FacebookLoginBtn
-      cssClass="btn btn-primary "
-      onError={onError}
-      onSuccess={onSuccess}
-    >
+    <FacebookLoginBtn cssClass="btn btn-primary">
       Login With FB
     </FacebookLoginBtn>
   );
