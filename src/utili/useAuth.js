f67c7cb53db890250b452b4692b227722ae23898
Muhammad Khalid <talhah.jan@gmail.com>
2022-08-22 14:18:02 -0700

    google on-tap sign in added

--- src/utili/useAuth.js
+++ src/utili/useAuth.js
@@ -10,28 +10,26 @@ const refreshJwt = async () => {
 };
 
 const useAuth = (token) => {
-  google.accounts.id.initialize({
-    client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-    callback: handleCallbackResponse,
-  });
-
-  google.accounts.id.prompt();
   const lastDate = Date.now() - 1 * 24 * 60 * 60 * 1000;
 
-  if (!token) return null;
+  if (!token || user.exp * 1000 < Date.now()) {
+    console.log(`jwt expired`);
+    localStorage.removeItem("token");
 
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.prompt();
+    return null;
+  }
   const user = jwt_decode(token);
 
   // if age of token is more than one day refresh it
   if (user.nbf * 1000 < lastDate) {
     console.log(`jwt need to be refreshed`);
     refreshJwt();
-  }
-
-  if (user.exp * 1000 < Date.now()) {
-    console.log(`jwt expired`);
-    localStorage.removeItem("token");
-    return null;
   }
 
   return user;
