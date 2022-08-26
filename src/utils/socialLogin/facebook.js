3b70eaf21de9e14713041c742cb19e0ca9c6e09d
Muhammad Khalid <talhah.jan@gmail.com>
2022-08-26 13:18:50 -0700

    facebook login added

--- src/utils/socialLogin/facebook.js
+++ src/utils/socialLogin/facebook.js
@@ -35,16 +35,19 @@ const FacebookLogin = ({ cssClass, btnText }) => {
   }, []);
 
   const SignIn = () => {
-    FB.login(function (response) {
-      if (response.authResponse) {
-        console.log("Welcome!  Fetching your information.... ");
-        FB.api("/me", function (response) {
-          console.log("Good to see you, " + response.name + ".");
-        });
-      } else {
-        console.log("User cancelled login or did not fully authorize.");
-      }
-    });
+    FB.login(
+      function (response) {
+        if (response.authResponse) {
+          console.log("Welcome!  Fetching your information.... ");
+          FB.api("/me", function (response) {
+            console.log("Good to see you, " + response.name + ".");
+          });
+        } else {
+          console.log("User cancelled login or did not fully authorize.");
+        }
+      },
+      { scope: "email,user_likes" }
+    );
   };
 
   return (
