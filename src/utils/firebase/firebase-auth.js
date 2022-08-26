0d80a8a2aeaa978474a8360c989ccaca1d670b52
Muhammad Khalid <talhah.jan@gmail.com>
2022-08-25 18:08:40 -0700

    firebase auth used for login

--- src/utils/firebase/firebase-auth.js
+++ src/utils/firebase/firebase-auth.js
@@ -4,9 +4,11 @@ import { onSuccessLogin as onSuccess, onErrorLogin as onError } from "../lib";
 const SocialMediaAuth = async (provider) => {
   return await signInWithPopup(auth, provider)
     .then((response) => {
+      console.log(response);
       return onSuccess(response);
     })
     .catch((error) => {
+      console.log(error);
       return onError(error);
     });
 };
