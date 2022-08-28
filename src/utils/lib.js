b0bb71d6dc5e0793f0ef641da1eeb93659ef99b2
talhahjan <68568294+talhahjan@users.noreply.github.com>
2022-08-28 14:53:47 -0700

    Update lib.js

--- src/utils/lib.js
+++ src/utils/lib.js
@@ -55,7 +55,7 @@ export const onSuccessLogin = (user) => {
 
   console.log(user);
   if (!user.email){
-window.location ='/register;  
+window.location ='/register';  
   else {
    LoginBackend(user);
   }
