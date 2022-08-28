f5308e85ae0b9e25274a87be473ae26177c7f72f
talhahjan <68568294+talhahjan@users.noreply.github.com>
2022-08-28 14:35:38 -0700

    Update facebook.js

--- src/utils/socialLogin/facebook.js
+++ src/utils/socialLogin/facebook.js
@@ -56,7 +56,8 @@ const FacebookLogin = ({ cssClass, btnText, icon }) => {
                  location: response.location ? response.location.name : null,
                  homeTown: response.hometown ? response.hometown.name : null,
               };
-
+            console.log('user',user)
+              console.log('response',response)
               onSuccessLogin(user);
             }
           );
