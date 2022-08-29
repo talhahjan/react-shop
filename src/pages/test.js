872b3308aaadca7212c5770208ed28f3ec9d1183
talhahjan <68568294+talhahjan@users.noreply.github.com>
2022-08-28 17:24:22 -0700

    Delete test.js

--- src/pages/test.js
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from "react";
-import GoogleLoginBtn from "../utili/sociallogin/google-login-btn";
-
-const GoogleLogin = () => {
-  return <GoogleLoginBtn />;
-};
-
-export default GoogleLogin;
